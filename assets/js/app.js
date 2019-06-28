0c39db4d755d7d5807ac647522fe34df96764700
Paul Casiraghi <paulcsrgh@gmail.com>
2019-06-28 18:09:18 +0200

    Ajout ENCORE + page carrer

---
+++ assets/js/app.js
@@ -0,0 +1,147 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you require will output into a single css file (app.css in this case)
+require('../css/app.css');
+require('waypoints/lib/jquery.waypoints.js')
+require('@loadingio/loading-bar')
+require('stellar.js')
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
+// const $ = require('jquery');
+
+// Appear/Dissapear nav
+$('.main-ctn-burger').click(function() {
+    $('.main-ctn-nav').toggleClass('main-ctn-nav-toggle')
+    $('.main-ctn-burger').toggleClass('toggle-burger')
+    $('.nav-menu').toggleClass('nav-menu-toggle')
+
+});
+
+var el = document.querySelector('.balayage')
+el.addEventListener("animationend", show, false)
+
+
+function show() {
+    $('.main-ctn-bigTitle, .main-ctn-burger, .main-ctn-dropdown ').addClass('show');
+};
+
+$(document).ready(function() {
+    $('.main-ctn-dropdown').on('click', function() {
+        var section = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(section).offset().top
+        }, 700);
+        return false;
+    });
+});
+
+
+//Appear skills
+$(document).ready(function() {
+    $('.ctn-skills-ctn-triangle').waypoint(function(direction) {
+        var t1 = new ldBar('.T-1')
+        var t2 = new ldBar('.T-2')
+        var t3 = new ldBar('.T-3')
+        var t4 = new ldBar('.T-4')
+        var t5 = new ldBar('.T-5')
+        var t6 = new ldBar('.T-6')
+        var t7 = new ldBar('.T-7')
+        var t8 = new ldBar('.T-8')
+        if (direction == 'down') {
+            t1.set(90)
+            t2.set(90)
+            t3.set(80)
+            t4.set(70)
+            t5.set(70)
+            t6.set(75)
+            t7.set(60)
+            t8.set(70)
+        } else {
+            t1.set(0)
+            t2.set(0)
+            t3.set(0)
+            t4.set(0)
+            t5.set(0)
+            t6.set(0)
+            t7.set(0)
+            t8.set(0)
+        }
+
+    }, {
+        offset: '70%'
+    });
+
+    $('.about-ctn-title').waypoint(function(direction) {
+        if (direction == 'down') {
+            $('.about-ctn-title').addClass('show');
+        } else {
+            $('.about-ctn-title').removeClass('show');
+        }
+    }, {
+        offset: '50%'
+    });
+
+    $('.about-ctn-big-ctn-bio').waypoint(function(direction) {
+        if (direction == 'down') {
+            $('.about-ctn-big-ctn-bio').addClass('show');
+        } else {
+            $('.about-ctn-big-ctn-bio').removeClass('show');
+        }
+    }, {
+        offset: '50%'
+    });
+
+    $('.ctn-skills-h2').waypoint(function(direction) {
+        if (direction == 'down') {
+            $('.ctn-skills-h2').addClass('show');
+        } else {
+            $('.ctn-skills-h2').removeClass('show');
+        }
+    }, {
+        offset: '50%'
+    });
+
+    $('.about-ctn-big-ctn-assets').waypoint(function(direction) {
+        if (direction == 'down') {
+            $('.about-ctn-big-ctn-assets').addClass('show');
+        } else {
+            $('.about-ctn-big-ctn-assets').removeClass('show');
+        }
+    }, {
+        offset: '90%'
+    });
+    $('.career').waypoint(function(direction) {
+        if (direction == 'down') {
+            $('.career').addClass('show');
+        } else {
+            $('.career').removeClass('show');
+        }
+    }, {
+        offset: '50%'
+    });
+
+
+
+
+
+});
+
+
+
+
+$(document).ready(function() {
+
+    $('.career-ctn-center-gallery-wrap').on('click', 'div.career-btn-reponsive', function() {
+
+        $(this).siblings('.item-ctn-title').toggleClass('item-ctn-title-hide-reponsive')
+        $(this).parents('.item').toggleClass('item-show-responsive')
+        $(this).siblings('.item-ctn-desc').toggleClass('item-ctn-desc-show-responsive')
+        $(this).children('.fa-chevron-down').toggleClass('chevron-rotate')
+
+    });
+});
--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -5,19 +5,31 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\SkillRepository;
+use App\Repository\ExperienceRepository;
+use App\Entity\Skill;
+use App\Entity\Experience;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+
+
 
 class FrontController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function home(SkillRepository $repo)
+    public function index(Request $request, ObjectManager $manager)
     {
-        $skill = $repo->findAll();
+        $skill = $this->getDoctrine()->getRepository(Skill::class)->findAll();
+        $exp = $this->getDoctrine()->getRepository(Experience::class)->findAll();
 
         return $this->render('front/index.html.twig',[
-            'controller_name' => 'PortfolioController',
             'skills' => $skill,
+            'exps' => $exp,
         ]);
     }
+
+
+
+
 }

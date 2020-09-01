a651c9b1f52edd26c8e68d17c1924fb65f70272f
CSRGH27 <universaltaskmail@gmail.com>
2020-09-01 22:46:05 +0200

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,231 +6,245 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
-require('../css/send.css');
-require('waypoints/lib/jquery.waypoints.js')
-require('@loadingio/loading-bar')
-require('stellar.js')
+require("../css/app.css");
+require("../css/send.css");
+require("waypoints/lib/jquery.waypoints.js");
+require("@loadingio/loading-bar");
+require("stellar.js");
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
 
-
-//Appartion menu 
+//Appartion menu
 $(document).ready(function () {
-    $('.main-ctn-burger').click(function () {
-        $('.main-ctn-nav').toggleClass('main-ctn-nav-toggle')
-        $('.main-ctn-burger').toggleClass('toggle-burger')
-        $('.nav-menu').toggleClass('nav-menu-toggle')
+  $(".main-ctn-burger").click(function () {
+    $(".main-ctn-nav").toggleClass("main-ctn-nav-toggle");
+    $(".main-ctn-burger").toggleClass("toggle-burger");
+    $(".nav-menu").toggleClass("nav-menu-toggle");
+  });
 
-    });
+  var el = document.querySelector(".balayage");
+  el.addEventListener("animationend", show, false);
 
-    var el = document.querySelector('.balayage')
-    el.addEventListener("animationend", show, false)
-
-
-    function show() {
-        $('.main-ctn-bigTitle, .main-ctn-burger, .main-ctn-dropdown ').addClass('show');
-    };
+  function show() {
+    $(".main-ctn-bigTitle, .main-ctn-burger, .main-ctn-dropdown ").addClass(
+      "show"
+    );
+  }
 });
-
-
 
 // Appear/Dissapear nav
 
-
 $(document).ready(function () {
-    $('.main-ctn-dropdown, .menu-link-1, .menu-link-2, .menu-link-3, .menu-link-4, .menu-link-5').on('click', function () {
-        var section = $(this).attr('href');
-        $('html, body').animate({
-            scrollTop: $(section).offset().top
-        }, 700);
-        return false;
-    });
+  $(
+    ".main-ctn-dropdown, .menu-link-1, .menu-link-2, .menu-link-3, .menu-link-4, .menu-link-5"
+  ).on("click", function () {
+    var section = $(this).attr("href");
+    $("html, body").animate(
+      {
+        scrollTop: $(section).offset().top,
+      },
+      700
+    );
+    return false;
+  });
 });
-
 
 //Appear skills + Waypoints
 $(document).ready(function () {
-    $('.ctn-skills-ctn-triangle').waypoint(function (direction) {
-        var t1 = new ldBar('.T-1')
-        var t2 = new ldBar('.T-2')
-        var t3 = new ldBar('.T-3')
-        var t4 = new ldBar('.T-4')
-        var t5 = new ldBar('.T-5')
-        var t6 = new ldBar('.T-6')
-        var t7 = new ldBar('.T-7')
-        var t8 = new ldBar('.T-8')
-        if (direction == 'down') {
-            t1.set(90)
-            t2.set(90)
-            t3.set(80)
-            t4.set(70)
-            t5.set(70)
-            t6.set(75)
-            t7.set(60)
-            t8.set(70)
-        } else {
-            t1.set(0)
-            t2.set(0)
-            t3.set(0)
-            t4.set(0)
-            t5.set(0)
-            t6.set(0)
-            t7.set(0)
-            t8.set(0)
-        }
+  $(".ctn-skills-ctn-triangle").waypoint(
+    function (direction) {
+      var t1 = new ldBar(".T-1");
+      var t2 = new ldBar(".T-2");
+      var t3 = new ldBar(".T-3");
+      var t4 = new ldBar(".T-4");
+      var t5 = new ldBar(".T-5");
+      var t6 = new ldBar(".T-6");
+      var t7 = new ldBar(".T-7");
+      var t8 = new ldBar(".T-8");
+      if (direction == "down") {
+        t1.set(90);
+        t2.set(90);
+        t3.set(80);
+        t4.set(70);
+        t5.set(70);
+        t6.set(75);
+        t7.set(60);
+        t8.set(70);
+      } else {
+        t1.set(0);
+        t2.set(0);
+        t3.set(0);
+        t4.set(0);
+        t5.set(0);
+        t6.set(0);
+        t7.set(0);
+        t8.set(0);
+      }
+    },
+    {
+      offset: "70%",
+    }
+  );
 
-    }, {
-        offset: '70%'
-    });
+  $(".about-ctn-title").waypoint(
+    function (direction) {
+      if (direction == "down") {
+        $(".about-ctn-title").addClass("show");
+      } else {
+        $(".about-ctn-title").removeClass("show");
+      }
+    },
+    {
+      offset: "50%",
+    }
+  );
 
-    $('.about-ctn-title').waypoint(function (direction) {
-        if (direction == 'down') {
-            $('.about-ctn-title').addClass('show');
-        } else {
-            $('.about-ctn-title').removeClass('show');
-        }
-    }, {
-        offset: '50%'
-    });
+  $(".about-ctn-big-ctn-bio").waypoint(
+    function (direction) {
+      if (direction == "down") {
+        $(".about-ctn-big-ctn-bio").addClass("show");
+      } else {
+        $(".about-ctn-big-ctn-bio").removeClass("show");
+      }
+    },
+    {
+      offset: "50%",
+    }
+  );
 
-    $('.about-ctn-big-ctn-bio').waypoint(function (direction) {
-        if (direction == 'down') {
-            $('.about-ctn-big-ctn-bio').addClass('show');
-        } else {
-            $('.about-ctn-big-ctn-bio').removeClass('show');
-        }
-    }, {
-        offset: '50%'
-    });
+  $(".ctn-skills-h2").waypoint(
+    function (direction) {
+      if (direction == "down") {
+        $(".ctn-skills-h2").addClass("show");
+      } else {
+        $(".ctn-skills-h2").removeClass("show");
+      }
+    },
+    {
+      offset: "70%",
+    }
+  );
 
-    $('.ctn-skills-h2').waypoint(function (direction) {
-        if (direction == 'down') {
-            $('.ctn-skills-h2').addClass('show');
-        } else {
-            $('.ctn-skills-h2').removeClass('show');
-        }
-    }, {
-        offset: '70%'
-    });
+  $(".about-ctn-big-ctn-assets").waypoint(
+    function (direction) {
+      if (direction == "down") {
+        $(".about-ctn-big-ctn-assets").addClass("show");
+      } else {
+        $(".about-ctn-big-ctn-assets").removeClass("show");
+      }
+    },
+    {
+      offset: "90%",
+    }
+  );
 
-    $('.about-ctn-big-ctn-assets').waypoint(function (direction) {
-        if (direction == 'down') {
-            $('.about-ctn-big-ctn-assets').addClass('show');
-        } else {
-            $('.about-ctn-big-ctn-assets').removeClass('show');
-        }
-    }, {
-        offset: '90%'
-    });
+  $(".career").waypoint(
+    function (direction) {
+      if (direction == "down") {
+        $(".item").addClass("item-show");
+      } else {
+        $(".item").removeClass("item-show");
+      }
+    },
+    {
+      offset: "50%",
+    }
+  );
 
-    $('.career').waypoint(function (direction) {
-        if (direction == 'down') {
-            $('.item').addClass('item-show');
-        } else {
-            $('.item').removeClass('item-show');
-        }
-    }, {
-        offset: '50%'
-    });
+  $(".portfolio").waypoint(
+    function (direction) {
+      if (direction == "down") {
+        $(".portfolio-card").addClass("portfolio-card-show");
+      } else {
+        $(".portfolio-card").removeClass("portfolio-card-show");
+      }
+    },
+    {
+      offset: "50%",
+    }
+  );
 
-    $('.portfolio').waypoint(function (direction) {
-        if (direction == 'down') {
-            $('.portfolio-card').addClass('portfolio-card-show');
-        } else {
-            $('.portfolio-card').removeClass('portfolio-card-show');
-        }
-    }, {
-        offset: '50%'
-    });
-
-    $('.contact-form').waypoint(function (direction) {
-        if (direction == 'down') {
-            $('.contact-form').addClass('contact-form-show');
-        } else {
-            $('.contact-form').removeClass('contact-form-show');
-        }
-    }, {
-        offset: '90%'
-    });
-
-
-
-
-
+  $(".contact-form").waypoint(
+    function (direction) {
+      if (direction == "down") {
+        $(".contact-form").addClass("contact-form-show");
+      } else {
+        $(".contact-form").removeClass("contact-form-show");
+      }
+    },
+    {
+      offset: "90%",
+    }
+  );
 });
-
-
 
 //Portfolio responsive
 $(document).ready(function () {
-
-    $('.career-ctn-center-gallery-wrap').on('click', 'div.career-btn-reponsive', function () {
-
-        $(this).siblings('.item-ctn-title').toggleClass('item-ctn-title-hide-reponsive')
-        $(this).parents('.item').toggleClass('item-show-responsive')
-        $(this).siblings('.item-ctn-desc').toggleClass('item-ctn-desc-show-responsive')
-        $(this).children('.fa-chevron-down').toggleClass('chevron-rotate')
-
-    });
+  $(".career-ctn-center-gallery-wrap").on(
+    "click",
+    "div.career-btn-reponsive",
+    function () {
+      $(this)
+        .siblings(".item-ctn-title")
+        .toggleClass("item-ctn-title-hide-reponsive");
+      $(this).parents(".item").toggleClass("item-show-responsive");
+      $(this)
+        .siblings(".item-ctn-desc")
+        .toggleClass("item-ctn-desc-show-responsive");
+      $(this).children(".fa-chevron-down").toggleClass("chevron-rotate");
+    }
+  );
 });
-
 
 //OUverture modal avec ajax plus loader
 
-
-
-
 $(document).ready(function () {
-
-    $('.portfolio-card-btn').click(function () {
-        var url = Routing.generate('project_show', {
-            'id': $(this).attr('id')
-        });
-        $.get(url, function (data) {
-            $(".modal-content").html(data);
-        });
+  $(".portfolio-card-btn").click(function () {
+    var url = Routing.generate("project_show", {
+      id: $(this).attr("id"),
     });
-
-    var modal = $("#Modal"); //your modal 
-
-    $(document).on({
-        ajaxStart: function () {
-            modal.find(".modal-content").html(""); //empty modal every ajaxstart
-            $('.load').show();
-            modal.modal("hide"); //hide
-
-        },
-        ajaxStop: function () {
-            $('.load').hide();
-            modal.modal("show"); //modal show
-        }
+    $.get(url, function (data) {
+      $(".modal-content").html(data);
     });
+  });
+
+  var modal = $("#Modal"); //your modal
+
+  $(document).on({
+    ajaxStart: function () {
+      modal.find(".modal-content").html(""); //empty modal every ajaxstart
+      $(".load").show();
+      modal.modal("hide"); //hide
+    },
+    ajaxStop: function () {
+      $(".load").hide();
+      modal.modal("show"); //modal show
+    },
+  });
 });
-
 
 //Form barre rouge
 $(document).ready(function () {
-    $('.form-name').keyup(function () {
-        if (this.value) {
-            $('.form-span-name').addClass('span-scale');
-        } else {
-            $('.form-span-name').removeClass('span-scale');
-        }
-    });
-    $('.form-mail').keyup(function () {
-        if (this.value) {
-            $('.form-span-mail').addClass('span-scale');
-        } else {
-            $('.form-span-mail').removeClass('span-scale');
-        }
-    });
-    $('.form-text').keyup(function () {
-        if (this.value) {
-            $('.form-span-text').addClass('span-scale');
-        } else {
-            $('.form-span-text').removeClass('span-scale');
-        }
-    });
+  $(".form-name").keyup(function () {
+    if (this.value) {
+      $(".form-span-name").addClass("span-scale");
+    } else {
+      $(".form-span-name").removeClass("span-scale");
+    }
+  });
+  $(".form-mail").keyup(function () {
+    if (this.value) {
+      $(".form-span-mail").addClass("span-scale");
+    } else {
+      $(".form-span-mail").removeClass("span-scale");
+    }
+  });
+  $(".form-text").keyup(function () {
+    if (this.value) {
+      $(".form-span-text").addClass("span-scale");
+    } else {
+      $(".form-span-text").removeClass("span-scale");
+    }
+  });
 });
---
+++ src/Migrations/Version20200901201138.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200901201138 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE experience (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, place VARCHAR(255) NOT NULL, city VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, date_begin VARCHAR(255) NOT NULL, date_end VARCHAR(255) NOT NULL, image VARCHAR(255) DEFAULT NULL, updated_at DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE fos_user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, username_canonical VARCHAR(180) NOT NULL, email VARCHAR(180) NOT NULL, email_canonical VARCHAR(180) NOT NULL, enabled TINYINT(1) NOT NULL, salt VARCHAR(255) DEFAULT NULL, password VARCHAR(255) NOT NULL, last_login DATETIME DEFAULT NULL, confirmation_token VARCHAR(180) DEFAULT NULL, password_requested_at DATETIME DEFAULT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', UNIQUE INDEX UNIQ_957A647992FC23A8 (username_canonical), UNIQUE INDEX UNIQ_957A6479A0D96FBF (email_canonical), UNIQUE INDEX UNIQ_957A6479C05FB297 (confirmation_token), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE project (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, content LONGTEXT NOT NULL, image VARCHAR(255) DEFAULT NULL, updated_at DATETIME DEFAULT NULL, link VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE project_skill (project_id INT NOT NULL, skill_id INT NOT NULL, INDEX IDX_4D68EDE9166D1F9C (project_id), INDEX IDX_4D68EDE95585C142 (skill_id), PRIMARY KEY(project_id, skill_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE skill (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, level INT NOT NULL, image VARCHAR(255) DEFAULT NULL, updated_at DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE project_skill ADD CONSTRAINT FK_4D68EDE9166D1F9C FOREIGN KEY (project_id) REFERENCES project (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE project_skill ADD CONSTRAINT FK_4D68EDE95585C142 FOREIGN KEY (skill_id) REFERENCES skill (id) ON DELETE CASCADE');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE project_skill DROP FOREIGN KEY FK_4D68EDE9166D1F9C');
+        $this->addSql('ALTER TABLE project_skill DROP FOREIGN KEY FK_4D68EDE95585C142');
+        $this->addSql('DROP TABLE experience');
+        $this->addSql('DROP TABLE fos_user');
+        $this->addSql('DROP TABLE project');
+        $this->addSql('DROP TABLE project_skill');
+        $this->addSql('DROP TABLE skill');
+    }
+}
